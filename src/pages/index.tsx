5bead3cd0cbdbc675846323b7e527322ebdec52f
Matias-DR <Matias-DR@outlook.com>
2024-01-15 17:46:17 -0300

    FC

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,47 @@
+import { useState } from "react"
+
+const knowledges = <div></div>
+const experience = <div></div>
+const education = <div></div>
+const contact = <div></div>
+const about = <p className='absolute bottom-0 right-0 border border-solid border-green-100'>
+  Sobre mí...
+</p>
+
 export default function Home() {
+  const [card, setCard] = useState<JSX.Element>(about)
+
   return <main className='w-full h-full flex justify-center items-center bg-zinc-950'>
-    <div className='w-[95%] h-[95%] p-6 bg-zinc-900 border border-solid border-zinc-500 text-zinc-200'>
-      <p className='text-6xl'>
+    <div className='relative w-[95%] h-[95%] p-6 bg-zinc-900 border border-solid border-zinc-500 text-zinc-200'>
+      <h1 className='text-6xl'>
         Matias Diz Rendani
-      </p>
+      </h1>
       <p className=''>
         Desarrollador Backedn & Frontend autónomo
       </p>
-      <p className=''>
-        Sobre mí...
-      </p>
+      <ul className=''>
+        <li
+          className=''
+          onClick={() => setCard(knowledges)}
+        >Conocimientos</li>
+        <li
+          className=''
+          onClick={() => setCard(experience)}
+        >Experiencia</li>
+        <li
+          className=''
+          onClick={() => setCard(education)}
+        >Educación</li>
+        <li
+          className=''
+          onClick={() => setCard(contact)}
+        >Contacto</li>
+        <li
+          className=''
+          onClick={() => setCard(about)}
+        >Sobre mí</li>
+      </ul>
+      {card}
     </div>
   </main>
 }
