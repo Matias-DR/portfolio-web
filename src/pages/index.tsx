1d95b39dd1e6a16b508ae669cac2f5e0fd46eb4e
Matias-DR <Matias-DR@outlook.com>
2024-01-15 17:35:08 -0300

    FC

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,4 @@
-
-
 export default function Home() {
-
-  const array = Array.from(
-    Array(57).keys(),
-    (x: number) => x % 2 === 0 ? '400' : '300'
-  )
-
   return <main className='w-full h-full flex justify-center items-center bg-zinc-950'>
     <div className='w-[95%] h-[95%] p-6 bg-zinc-900 border border-solid border-zinc-500 text-zinc-200'>
       <p className='text-6xl'>
